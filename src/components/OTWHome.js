e52d4e61e6797a4226ee289dec95b2b6b2b1faff
Eric Lingren <ericlingren@gmail.com>
2019-09-14 14:35:56 -0600

    adding data for banding levels 0-3

--- src/components/OTWHome.js
+++ src/components/OTWHome.js
@@ -7,11 +7,10 @@ function OTWHome() {
             <div className='page-wrapper'>
                 <h1 className='page-title'> Over The Wire Home </h1>
                 <p>
-                    The OverTheWire project is a gameified site that focused on learning Linux, networking and security concepts through shell interactions.  These concepts are broken up in to wargames and further broken up into levels.  You can find the full details of the project here: <a target='_blank' rel="noopener noreferrer" href='http://overthewire.org/wargames/'>overthewire.org/wargames</a>
+                    The OverTheWire project is a gameified site that focused on learning Linux, networking and security concepts through shell interactions.  These concepts are broken up in to wargames and further broken up into levels.  You can find the full details of how this project works here: <a target='_blank' rel="noopener noreferrer" href='http://overthewire.org/wargames/'>overthewire.org/wargames</a>
                 </p>
-                <p>
-                    This site is a walkthrough of my progress with these wargames.
-                </p>
+                <p> This site is my walkthrough of the progress I am making with these wargames. </p>
+                <p> Click a wargame on the left to start exploring. </p>
             </div>
         </div>
     );
--- src/components/bandit/BanditLevelComponent.js
+++ src/components/bandit/BanditLevelComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import '../GlobalStyles.css'
 import BanditNavbar from './BanditNavbar'
+import Prism from "prismjs";
+import '../GlobalStyles.css'
+import "../prism.css";
 
 function BanditLevelComponent(props) {
     console.log(props.levelData);
@@ -9,10 +11,25 @@ function BanditLevelComponent(props) {
             <BanditNavbar />
             <div className='page-wrapper'>
                 <h1 className='page-title'> Bandit Level {props.levelData.level} </h1>
+
                 <h2 className='section-heading'> Level Goal: </h2>
                 <p> {props.levelData.goal} </p>
+
                 <h2 className='section-heading'> Commands you may need to solve this level: </h2>
                 <p> {props.levelData.commands} </p>
+                
+                <h2 className='section-heading'> Terminal Walkthrough: </h2>
+                <img src={`${props.levelData.image}`} alt='screenshot'/>
+
+                <h2 className='section-heading'> Answer: </h2>
+                <pre className='code-wrapper'>
+                    <code className="language-javascript">
+                        {props.levelData.answer}
+                    </code>
+                </pre>
+
+                <h2 className='section-heading'> What I learned: </h2>
+                <p> {props.levelData.takeaways} </p>
             </div>
         </div>
     );
